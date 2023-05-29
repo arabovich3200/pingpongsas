3296f5a4a5428c582235542827900e684493754e
kokushibo456 <134778275+arabovich3200@users.noreply.github.com>
2023-05-29 18:01:44 +0300

    Update main.py
    add class P(Player)

--- main.py
+++ main.py
@@ -9,3 +9,16 @@ class GS(sprite.Sprite):
         self.rect.y = player_y
     def res():
         w.blit(self.image, (self.rect.x, self.rect.y))
+class P(GS):
+    def up_r(self):
+        k = key.get_pressed()
+        if k[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if k[K_DOWN] and self.rect.y < w_height - 80:
+            self.rect.y += self.speed
+    def up_l(self):
+        k = key.get_pressed()
+        if k[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if k[K_s] and self.rect.y < w_height - 80:
+            self.rect.y += self.speed
