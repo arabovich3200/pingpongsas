f25c62443bfc132446beef2f334c710495df0bda
kokushibo456 <134778275+arabovich3200@users.noreply.github.com>
2023-05-29 18:37:32 +0300

    Update main.py
    global changing

--- main.py
+++ main.py
@@ -3,20 +3,19 @@ b = (200, 255, 255)
 w_width = 600
 w_height = 300
 w = display.set_mode((w_width, w_height))
-w.fill(b)
 g = True
 f = False
 clock = time.Clock()
 FPS = 60
 class GS(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+    def __init__(self, player_image, player_x, player_y, player_speed, width, height):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (wight, height)) 
+        self.image = transform.scale(image.load(player_image), (width, height)) 
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
-    def res():
+    def res(self):
         w.blit(self.image, (self.rect.x, self.rect.y))
 class P(GS):
     def up_r(self):
@@ -31,3 +30,23 @@ class P(GS):
             self.rect.y -= self.speed
         if k[K_s] and self.rect.y < w_height - 80:
             self.rect.y += self.speed
+r1 = P('rocket1.png', 30, 200, 4, 50, 150)
+r2 = P('rocket2.png', 520, 200, 4, 50, 150)
+ba = GS('ball.png', 200, 200, 4, 50, 50)
+speed_x = 3
+speed_y = 3
+while g:
+    for e in event.get():
+        if e.type == QUIT:
+            g = False
+    if not f:
+        w.fill(b)
+        r1.up_r()
+        r2.up_l()
+        ba.rect.x += speed_x
+        ba.rect.y += speed_y
+        r1.res()
+        r2.res()
+        ba.res()
+    display.update()
+    clock.tick(FPS)
